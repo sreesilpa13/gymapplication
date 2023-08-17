8ff4d4eec43d588ffcf64ff605b9f7656eda922c
Srinivas Basha <srinivas_basha@epam.com>
2023-08-17 23:02:54 +0530

    Initial commit

---
+++ gymnotificationservice/src/main/java/com/epam/NotificationController.java
@@ -0,0 +1,15 @@
+package com.epam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Gym-Application works";
+    }
+}
